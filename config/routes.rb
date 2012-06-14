44d3788fd636bd55ead829b6e94f61840102c92e
Sriram Balasubramanian <ramkalari@gmail.com>
2012-06-14 23:16:23 +0530

    adds store rest api

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,14 @@ Spree::Core::Engine.routes.append do
   namespace :admin do
     resources :stores
   end
+
+  namespace :api do
+    scope :module => :v1 do
+      resources :stores
+    end
+  end
+  
+  
 end
+
+
