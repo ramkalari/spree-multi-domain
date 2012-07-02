5502c0f0a1d75e813a5e89a154f2a0ad53feb0fd
Sriram Balasubramanian <ramkalari@gmail.com>
2012-07-02 13:08:26 +0530

    fix syntax error on order decorator

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -5,7 +5,7 @@ Spree::Order.class_eval do
   def deliver_order_confirmation_email
     begin
       Spree::OrderMailer.confirm_email(self).deliver
-      Spree::OrderMailer.confirm_email_to_stores(self).each |message| do
+      Spree::OrderMailer.confirm_email_to_stores(self).each do |message|
         message.deliver
       end
     rescue Exception => e
