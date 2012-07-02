32983972849230d3e3ff1422f011d380385331f3
Sriram Balasubramanian <ramkalari@gmail.com>
2012-07-02 12:18:03 +0530

    send new order mail to stores

--- app/mailers/spree/order_mailer.rb
+++ app/mailers/spree/order_mailer.rb
@@ -17,10 +17,15 @@ class Spree::OrderMailer < ActionMailer::Base
     # use group by to get line items for each store and store in into a hash
     # foe every key in the hash, load the store, get its email and send the lineitems to the template and the email
     @order = order
-    subject = (resend ? "[RESEND] " : "")
-    subject += "#{Spree::Config[:site_name]} Order Confirmation ##{order.number}"
-    mail(:to => order.email,
-           :subject => subject)
+    line_items_by_store = order.line_items.group_by {|line_item| line_item.store_id}
+    line_items_by_store.each do |key, value|
+        @line_items = value
+        subject = (resend ? "[RESEND] " : "")
+        subject += "#{Spree::Config[:site_name]} New Order ##{order.number}"
+        mail(:to => order.email,
+             :subject => subject)
+
+    end
   end
 
   def cancel_email(order, resend=false)
